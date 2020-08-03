21b16afded507324dbdec1b4630b515246de87af
Mariel <marielcarrillo@MacBook-Pro-de-Mariel.local>
2020-08-03 10:25:28 -0500

    Updates

---
+++ precache-manifest.94ece4a33f237f3eac351a7bf2a7aee1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7454904edcebe860f9afd639ba098795",
+    "url": "/burger-queen/index.html"
+  },
+  {
+    "revision": "79de5911eb521a930101",
+    "url": "/burger-queen/static/css/2.3cb76441.chunk.css"
+  },
+  {
+    "revision": "571957c3d15fec294e51",
+    "url": "/burger-queen/static/css/main.49429f36.chunk.css"
+  },
+  {
+    "revision": "79de5911eb521a930101",
+    "url": "/burger-queen/static/js/2.205e4390.chunk.js"
+  },
+  {
+    "revision": "544111c69119227bbf075a65771ccdb1",
+    "url": "/burger-queen/static/js/2.205e4390.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "571957c3d15fec294e51",
+    "url": "/burger-queen/static/js/main.a0b7f7b3.chunk.js"
+  },
+  {
+    "revision": "8fee150282e663ed1459",
+    "url": "/burger-queen/static/js/runtime-main.55400cb0.js"
+  },
+  {
+    "revision": "fe689983fa1c8e0af437ac3be4ddae44",
+    "url": "/burger-queen/static/media/Victoria Melody.fe689983.otf"
+  },
+  {
+    "revision": "98d2f97305ab25d4511982e73740922e",
+    "url": "/burger-queen/static/media/futura light bt.98d2f973.ttf"
+  },
+  {
+    "revision": "ee64fb9d3f1ba2333e1b489283925bce",
+    "url": "/burger-queen/static/media/futura medium bt.ee64fb9d.ttf"
+  },
+  {
+    "revision": "b38416ca6d2f5ff7b4b85d5abdc9d324",
+    "url": "/burger-queen/static/media/logoPrim.b38416ca.png"
+  },
+  {
+    "revision": "20cf657de3e137e7caac18dad59a33f3",
+    "url": "/burger-queen/static/media/logoSmall.20cf657d.png"
+  }
+]);
